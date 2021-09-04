1867bee53ccfed7703ca0fea2e8182540cf8d728
Kiran Anand <liteninkiran@hotmail.com>
2021-09-04 17:54:48 +0100

    Adding home page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,8 +12,7 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {
+        User::factory(10)->create();
     }
 }
